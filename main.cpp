293446d088c367ff4c0f1dfd6b98078469c355c5
Hugo Regibo <hugo@humanfocus.be>
2017-10-02 23:57:20 +0200

    done ex 1.8

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 
 int main(int argc, char* argv[]) {
+  // valid
+  std::cout << "/*";
+  // valid
+  std::cout << "*/";
+  // invalid
+//  std::cout << /* "*/" */;
+  // valid
+  std::cout << /* "*/" /* "/* " */;
   return 0;
 }
