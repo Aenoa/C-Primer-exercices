9000d46580ad0cc5c58ca1b44b1e5c5c3a90699b
Hugo Regibo <hugo@humanfocus.be>
2017-10-02 23:08:53 +0200

    done 1.6

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 
 int main(int argc, char* argv[]) {
+  // This is invalid, there is a ; after each line but no std::cout at start; either both or none are needed (except for final ;).
   return 0;
 }
