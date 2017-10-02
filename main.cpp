be3119ec8894190d1a37cfc722aedb8a0ef8a237
Hugo Regibo <hugo@humanfocus.be>
2017-10-02 23:51:24 +0200

    end 1.6

--- main.cpp
+++ main.cpp
@@ -2,5 +2,8 @@
 
 int main(int argc, char* argv[]) {
   // This is invalid, there is a ; after each line but no std::cout at start; either both or none are needed (except for final ;).
+  std::cout << "the sum of " << v1;
+            << " and " << v2;
+            << " is " << v1 + v2 << std::endl;
   return 0;
 }
