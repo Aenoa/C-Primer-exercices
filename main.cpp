f90f161138d0b34301fc39d3a31b575da5a8464c
Hugo Regibo <hugo@humanfocus.be>
2017-10-02 23:04:19 +0200

    finished ex 1.4

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 
 int main(int argc, char* argv[]) {
+  int v1 = 0, v2 = 0;
+  std::cin >> v1 >> v2;
+  std::cout << "product of " << v1 << " and " << v2 << " = " << v1 * v2 << std::endl;
   return 0;
 }
