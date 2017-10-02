fd2ae2672d2ed2b04ccb962bb7da045b93d97c24
Hugo Regibo <hugo@humanfocus.be>
2017-10-02 23:53:09 +0200

    finished ex 1.7

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 
+/*
+* Main method 
+* returns a status code for the OS
+* gets a arg count, and a arg list as array
+*/
 int main(int argc, char* argv[]) {
+  // another comment, monoline
   std::cout << "/*";
   std::cout << "*/"; 
   return 0;
